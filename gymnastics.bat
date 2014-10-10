485d1c298b8352b36288291998caf8ea541940a1
Ilfat <ilfat.abdullin@tatar.ru>
2014-10-10 15:22:20 +0400

    extend sleep to 3600

--- gymnastics.bat
+++ gymnastics.bat
@@ -1,7 +1,7 @@
 @echo off
 
 :whiletrue
-	sleep 10
+	sleep 3600
 	echo "It's time to eye gymnastics!"
 	explorer http://s1.developerslife.ru/public/images/gifs/67dff81a-f812-4bce-88da-215b7a65eb05.gif
 goto :whiletrue
