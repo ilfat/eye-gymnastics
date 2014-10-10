d9751384fcf9d1626b503b63f66450c96f205e97
Ilfat <ilfat.abdullin@tatar.ru>
2014-10-10 15:19:23 +0400

    added windows batch

---
+++ gymnastics.bat
@@ -0,0 +1,8 @@
+@echo off
+
+:whiletrue
+	sleep 10
+	echo "It's time to eye gymnastics!"
+	explorer http://s1.developerslife.ru/public/images/gifs/67dff81a-f812-4bce-88da-215b7a65eb05.gif
+goto :whiletrue
+
