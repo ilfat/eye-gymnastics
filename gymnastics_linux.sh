8dbec9c992f3c6ab9b984161c9b2fc32418a2435
Олег Степанищев <Oleg.Stepanischev@tatar.ru>
2014-10-10 15:25:54 +0400

    added linux xdg-open

---
+++ gymnastics_linux.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+while true
+	do
+		sleep 3600	
+		echo "It's time to eye gymnastics!"
+		xdg-open http://s1.developerslife.ru/public/images/gifs/67dff81a-f812-4bce-88da-215b7a65eb05.gif
+	done
